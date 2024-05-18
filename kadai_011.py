6da71fc9e207823d6b72b47d98be46746691f6e2
yudemaru <141104627+yudemaru@users.noreply.github.com>
2024-05-18 20:25:17 +0900

    Add files via upload

---
+++ kadai_011.py
@@ -0,0 +1,9 @@
+array = ["水","金","地","火","木","土","天","海","冥"]
+for solar_system in array:
+    print(solar_system)
+    
+index = 0
+
+while index < len(array):
+    print(array[index])
+    index += 1
